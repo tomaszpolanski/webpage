1eda750f0bae4710fc5946a3e67c3fb629092115
Tomasz Polański <polanski.tomek@gmail.com>
2017-06-06 21:30:36 +0200

    Downloading image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { GithubService } from '../shared/github.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  avatar: String = '';
+
+  constructor(private github: GithubService) {
+    github.githubUser().subscribe((it) => this.avatar = it.avatar_url );
+  }
+
+  ngOnInit(): void {
+  }
   title = 'app works!';
 }
---
+++ src/shared/github.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'RxJS/Rx';
+import 'rxjs/add/operator/map';
+
+export interface User {
+  avatar_url: string;
+}
+
+@Injectable()
+export class GithubService {
+
+  constructor(private http: Http) { }
+
+  public githubUser() {
+    return this.http
+            .get(`https://api.github.com/users/tomaszpolanski`)
+            .map(response => response.json());
+  }
+}
