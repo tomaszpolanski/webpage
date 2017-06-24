eb9996ecd92be9723bd908e53d6f19b546364095
Tomasz Polański <polanski.tomek@gmail.com>
2017-06-24 21:21:11 +0200

    Adding tests

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,13 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MdProgressSpinnerModule, MdCardModule, MdButtonModule, MdTooltipModule } from '@angular/material';
 import { Observable } from 'rxjs/Observable';
+import { By } from '@angular/platform-browser';
 import 'rxjs/add/observable/empty';
+import 'rxjs/add/observable/of';
 
 import { AboutComponent } from './about.component';
 import { BadgeComponent } from '../../shared/badge/badge.component';
 import { GithubService } from '../../shared/github.service';
 import { PrismicService, Section, Contact, Programming } from '../../shared/prismic.service';
 import { GoogleAnalytics } from '../../shared/google-analytics.service';
+
+const TEXT_SECTION_TITLE = 'Some section';
 
 class MockPrismicService {
   getContacts(): Observable<Section<Contact>> {
@@ -17,7 +21,14 @@ class MockPrismicService {
     return Observable.empty();
   }
   getAbout(): Observable<Section<String>[]> {
-    return Observable.empty();
+    const section: Section<String> = {
+      order: 0,
+      size: 'short',
+      kind: 'text',
+      title: TEXT_SECTION_TITLE,
+      content: ['Content'],
+    };
+    return Observable.of([section]);
   }
 
 }
@@ -52,5 +63,11 @@ describe('AboutComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('creates text sections', () => {
+    const sections = fixture.debugElement.query(By.css('.short'));
+
+    expect(sections.nativeElement.querySelector('h2').textContent).toBe(TEXT_SECTION_TITLE);
   });
 });
